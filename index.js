9fd1e38e8fba649dd757e806c237e1c5f0d639ce
晒太阳 <oncwnuMK8eslzwwto-ueCCDtVLK4@git.weixin.qq.com>
2019-06-21 11:43:50 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require('fs-extra')
+const path = require('path')
+const Swagger = require('swagger-client')
+const Handlebars = require('handlebars')
+const camelCase = require('camelcase')
+const beautify = require('js-beautify').js_beautify
+
+function getTemplate (templateData) {
+  const templateStr = fs.readFileSync(path.join(__dirname, 'template.hbs'), 'utf-8')
+  return Handlebars.compile(templateStr)(templateData)
+}
+
+/**
+ *
+ * @param docUrl {String} Swagger json url
+ * @param absolute path {String} Relative output path
+ */
+function generateFile ({ docUrl, filepath }) {
+  Swagger.resolve({ url: docUrl }).then(res => {
+    const apis = []
+    Object.entries(res.spec.paths).forEach(([path, methodObj]) => {
+      Object.entries(methodObj).forEach(([method, { operationId, description }]) => {
+        apis.push({
+          description,
+          path: path.replace('{id}', '${params.id}'),
+          name: camelCase(operationId),
+          method: method.toUpperCase(),
+          hasRequestBody: ['put', 'post'].includes(method),
+          hasIdInPath: /{id}/.test(path)
+        })
+      })
+    })
+    const result = getTemplate({ apis })
+    fs.outputFileSync(filepath, beautify(result, {
+      indent_size: 2,
+      'preserve-newlines': true
+    }))
+  })
+}
+
+module.exports = generateFile
